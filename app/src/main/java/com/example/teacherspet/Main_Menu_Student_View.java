2a5c8aa0672ec78138942b7ca321b5ad6ca9b8bc
mikamorgan <mmorgan@archercityisd.net>
2021-02-05 21:55:56 -0600

    Initial commit- Login UI partially complete, student home screen with 4 linked buttons, 4 activity pages for student actions

---
+++ app/src/main/java/com/example/teacherspet/Main_Menu_Student_View.java
@@ -0,0 +1,70 @@
+package com.example.teacherspet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main_Menu_Student_View extends AppCompatActivity {
+
+    public void openAttendance() {
+        Intent intent = new Intent(this, Attendance.class);
+        startActivity(intent);
+    }
+
+    public void openLessons() {
+        Intent intent = new Intent(this, Lessons.class);
+        startActivity(intent);
+    }
+
+    public void openSelf_Study() {
+        Intent intent = new Intent(this, Self_Study.class);
+        startActivity(intent);
+    }
+
+    public void openProgress() {
+        Intent intent = new Intent(this, Progress.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_student);
+
+        final Button attendanceButton = findViewById(R.id.attendance);
+        final Button lessonsButton = findViewById(R.id.lessons);
+        final Button self_studyButton = findViewById(R.id.self_study);
+        final Button progressButton = findViewById(R.id.progress);
+
+        attendanceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAttendance();
+            }
+        });
+
+        lessonsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLessons();
+            }
+        });
+
+        self_studyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSelf_Study();
+            }
+        });
+
+        progressButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openProgress();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/teacherspet/ui/login/LoggedInUserView.java
@@ -0,0 +1,34 @@
+package com.example.teacherspet.ui.login;
+
+import android.content.Intent;
+
+import com.example.teacherspet.Main_Menu_Student_View;
+import com.example.teacherspet.Progress;
+
+/**
+ * Class exposing authenticated user details to the UI.
+ */
+class LoggedInUserView {
+
+    private String displayName;
+    //... other data fields that may be accessible to the UI
+
+    LoggedInUserView(String displayName) {
+        this.displayName = displayName;
+    }
+
+    String getDisplayName() {
+        return displayName;
+    }
+}
+
+/*
+class LoggedInUserView {
+    public void openMain_Menu_Student_View() {
+        Intent intent = new Intent(this, Main_Menu_Student_View.class);
+        startActivity(intent);
+    }
+
+    openMain_Menu_Student_View();
+
+ */
