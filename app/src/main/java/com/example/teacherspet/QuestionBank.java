09e2d4a5737af79a8d703bb941956a7a0e860f10
mikamorgan <mmorgan@archercityisd.net>
2021-02-10 14:55:04 -0600

    Add 4th answer choice and comment Question Bank class

--- app/src/main/java/com/example/teacherspet/QuestionBank.java
+++ app/src/main/java/com/example/teacherspet/QuestionBank.java
@@ -1,21 +1,46 @@
 package com.example.teacherspet;
 
 public class QuestionBank {
+
+    /*************************************************
+     * Add questions to the question bank here :)
+     * SEPARATE ORDER BY SUBJECT!
+     * Use comments to delineate subject indices.
+     * We can pass in random a to select from
+     * specific index ranges for each subject.
+     ************************************************/
     private String Questions [] = {
             "Enter first question here",
             "Enter second question here"
     };
 
+    /*************************************************
+     * Add all 4 possible answer choices here :)
+     * VERY IMPORTANT!
+     * The index of the answer choices has to match
+     * the index of the corresponding question!
+     ************************************************/
     private String Answers [][] = {
-            {"Choice A", "Choice B", "Choice C"},
-            {"Choice A", "Choice B", "Choice C"}
+            {"Choice A", "Choice B", "Choice C", "Choice D"},
+            {"Choice A", "Choice B", "Choice C", "Choice D"}
     };
 
+    /**************************************************
+     * Identify the one correct answer choice here :)
+     * This choice should be one of the ones already
+     * listed in Answers array!
+     * VERY IMPORTANT!
+     * The index of the correct answer choice should
+     * match the corresponding question and choices!
+     *************************************************/
     private String CorrectAnswers [] = {
-            "Choice B",
+            "Choice C",
             "Choice A"
     };
 
+    /**************************************************
+     * Accessor methods
+     *************************************************/
     public String getQuestion(int a){
             String question = Questions[a];
             return question;
@@ -34,6 +59,11 @@ public class QuestionBank {
     public String getChoice3(int a){
         String choice3 = Answers[a][2];
         return choice3;
+    }
+
+    public String getChoice4(int a){
+        String choice4 = Answers[a][3];
+        return choice4;
     }
 
     public String getCorrectAnswer(int a){
