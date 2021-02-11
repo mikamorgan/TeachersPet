b166f0bc2d73bb8f4b37e198e336709e6516430c
mikamorgan <mmorgan@archercityisd.net>
2021-02-10 22:32:19 -0600

    Create classes and activity pages for each skills check subject

---
+++ app/src/main/java/com/example/teacherspet/SC_Binary_Trees.java
@@ -0,0 +1,14 @@
+package com.example.teacherspet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SC_Binary_Trees extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_s_c__binary__trees);
+    }
+}
---
+++ app/src/main/java/com/example/teacherspet/SC_Lists.java
@@ -0,0 +1,14 @@
+package com.example.teacherspet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SC_Lists extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_s_c__lists);
+    }
+}
