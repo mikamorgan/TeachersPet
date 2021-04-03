61b001410c0047a971fe656db081ef6f3a8c9998
mikamorgan <mmorgan@archercityisd.net>
2021-04-03 08:33:26 -0500

    SIGNUP WORKS!!!!!

--- app/src/main/java/com/example/teacherspet/ui/login/SignUpUserView.java
+++ app/src/main/java/com/example/teacherspet/ui/login/SignUpUserView.java
@@ -12,10 +12,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.teacherspet.R;
@@ -47,10 +51,7 @@ public class SignUpUserView extends AppCompatActivity {
         EditText passwordEditText = findViewById(R.id.editTextTextPassword);
         EditText passwordverifyEditText = findViewById(R.id.editTextTextPassword2);
 
-        final Button loginButton = findViewById(R.id.login);
-        final Button emailverifyButton = findViewById(R.id.signup); // inside on create
-
-        String name = String.valueOf(nameEditText.getText());
+        /*String name  = nameEditText.getText().toString();
         String email = emailEditText.getText().toString();
         String temp1 = passwordEditText.getText().toString();
         String temp2 = passwordverifyEditText.getText().toString();
@@ -60,22 +61,81 @@ public class SignUpUserView extends AppCompatActivity {
         {
            password = temp1;
         }
+
+        //TextWatcher checks for user input in text entry fields
+        TextWatcher afterTextChangedListener = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // ignore
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // ignore
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String name  = nameEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String temp1 = passwordEditText.getText().toString();
+                String temp2 = passwordverifyEditText.getText().toString();
+                String password = "";
+
+                if (temp1.equals(temp2))
+                {
+                    password = temp1;
+                }
+
+                Toast.makeText(getApplicationContext(), " ATC email" + email, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "ATC pw" + password, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "ATC name" + name, Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        //On creation, allow the user to enter text for un and pw
+        //Hide pw text as it is entered. Only check for pw changes if login fails
+        emailEditText.addTextChangedListener(afterTextChangedListener);
+        passwordEditText.addTextChangedListener(afterTextChangedListener);
+        nameEditText.addTextChangedListener(afterTextChangedListener);
+
+
+
         //System.out.println("Name: " + name);
         System.out.println("Email: " + email);
         System.out.println("Pw: " + password);
-        Log.d("","Name " + name);
+        Log.i("","Name " + name);
+        Log.i("","PW " + password);
+        Log.i("","Email " + email);
 
         User user = new User(email, password, name);
+
+         */
+
+        final Button loginButton = findViewById(R.id.login);
+        final Button emailverifyButton = findViewById(R.id.signup); // inside on create
 
         /****************************************************************************
          *  OnClickListener checks to see if a button is pressed
          *
          *  If the button is pressed, open the sign up  method page
          ***************************************************************************/
-        String finalPassword = password;
         emailverifyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String name  = nameEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String temp1 = passwordEditText.getText().toString();
+                String temp2 = passwordverifyEditText.getText().toString();
+                String password = "";
+
+                if (temp1.equals(temp2))
+                {
+                    password = temp1;
+                }
+
+                String finalPassword = password;
 
                 //Start ProgressBar first (Set visibility VISIBLE)
                 Handler handler = new Handler(Looper.getMainLooper());
@@ -97,8 +157,8 @@ public class SignUpUserView extends AppCompatActivity {
                         data[2] = name;
                         data[3] = "";
                         data[4] = "";
-                        Toast.makeText(getApplicationContext(), email, Toast.LENGTH_SHORT).show();
-                        Toast.makeText(getApplicationContext(), finalPassword, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "email" + email, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "pw" + finalPassword, Toast.LENGTH_SHORT).show();
                         Toast.makeText(getApplicationContext(), "name" + name, Toast.LENGTH_SHORT).show();
 
                         PutData putData = new PutData("http://192.168.1.138/LoginRegister/signup.php", "POST", field, data);
